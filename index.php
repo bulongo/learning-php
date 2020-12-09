d91f15ea2011168a58f5e4b334a894d626814fe1
Bulongo Mukkuli <bmukkuli@gmail.com>
2020-12-09 22:14:49 +0200

    Why isn't this working

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$x = array(
+    'bulongo' =>array($age=>31,$sex=>'Male',$job=>'coding'),
+    'moonga'  =>array($age=>24,$sex=>'Female',$job=>'PR Person'),
+    'mulube'  =>array($age=>20,$sex=>'Male',$job=>'PR Person'),
+    'kasamba' =>array($age=>18,$sex=>'Male',$job=>'Editor General'),
+    'dalitso' =>array($age=>12,$sex=>'Male',$job=>'school'),
+    'chiseh'  =>array($age=>25,$sex=>'Male',$job=>'sales manger'));
+
+echo $x[1];
+
+?>
